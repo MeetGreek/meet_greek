72afcc4035ab722552ced19bb1af86dfe91f850a
Georgios Lymperopoulos <glympe77@gmail.com>
2016-12-05 16:19:31 +0200

    Install facebook plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Data } from '../../providers/data';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+export class HomePage {
+  chatMessage: string = '';
+  messages: any = [];
+  constructor(public dataService: Data) {
+
   }
 
+  sendMessage(): void {
+
+  }
 }
